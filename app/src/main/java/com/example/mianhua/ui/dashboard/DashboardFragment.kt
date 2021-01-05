f9548d7f79b5d1e83fe36c4b81823df198c2db60
slgxmh <slgxmh@foxmail.com>
2021-01-05 23:10:10 +0800

    init

---
+++ app/src/main/java/com/example/mianhua/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,25 @@
+package com.example.mianhua.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.mianhua.R
+
+class DashboardFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: DashboardViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel =
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_ai, container, false)
+        return root
+    }
+}
