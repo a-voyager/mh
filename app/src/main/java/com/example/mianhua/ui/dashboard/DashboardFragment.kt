606abe530d1350188f7593e839b07a3dbe8d9400
voyagerwu <voyagerwu@tencent.com>
2021-01-06 11:32:35 +0800

    [feature] camera capture.

--- app/src/main/java/com/example/mianhua/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/mianhua/ui/dashboard/DashboardFragment.kt
@@ -1,5 +1,6 @@
 package com.example.mianhua.ui.dashboard
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.mianhua.R
+import com.example.mianhua.ui.camera.CameraActivity
 
 class DashboardFragment : Fragment() {
 
@@ -22,4 +24,23 @@ class DashboardFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_ai, container, false)
         return root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<View>(R.id.button).setOnClickListener {
+            CameraActivity.launch(this)
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val uri = CameraActivity.parse(requestCode, resultCode, data)
+        val context = context
+        if (uri != null && context != null) {
+            dashboardViewModel.onTakePhotoSucceed(context, uri)
+            return
+        }
+    }
+
 }
--- app/src/main/java/com/example/mianhua/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/mianhua/ui/dashboard/DashboardViewModel.kt
@@ -1,10 +1,25 @@
 package com.example.mianhua.ui.dashboard
 
+import android.content.Context
+import android.net.Uri
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class DashboardViewModel : ViewModel() {
+
+    fun onTakePhotoSucceed(context: Context, uri: Uri) {
+        val bytes = context.contentResolver.openInputStream(uri)?.use {
+            it.readBytes()
+        } ?: return
+        processImageBytes(bytes)
+    }
+
+    private fun processImageBytes(bytes: ByteArray) {
+        // 处理算法逻辑
+        Log.d("DashboardViewModel", "bytes size: ${bytes.size}")
+    }
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
